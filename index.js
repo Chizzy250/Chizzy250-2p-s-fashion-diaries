1dd3b44d7fd5163af9ed19d70f3c0559126e5838
Chizzy250 <104734604+Chizzy250@users.noreply.github.com>
2022-06-17 11:54:43 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+/* assigning a variable name to the text by using its Id */
+
+let myDiv = document.getElementById("myDiv");
+
+/* defining the function to display text when button is clicked */
+
+function display() {
+    myDiv.style.display = "block"; /* and then the text appears */
+}
